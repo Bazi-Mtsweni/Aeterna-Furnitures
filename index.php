d58130ce5c5fced2b175e904e29458149a201685
Bazi-Mtsweni <bmtsweni4@gmail.com>
2024-10-10 18:10:26 +0200

    File structure and basic style setup

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aeterna Furnitures - Home</title>
 
     <!-- CSS -->
-     <link rel="stylesheet" href="./css/styles.css">
-     <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/index.css">
 
-     <!-- JS -->
-      <script src="./js/index.js"></script>
-</head>
-<body>
+    <!-- JS -->
+    <script src="./js/index.js"></script>
+
+    <!-- CDNs -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
+</head>
+
+<body>
+
 </body>
+
 </html>
